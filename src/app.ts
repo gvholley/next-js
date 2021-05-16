c57f05b8e09fe73db25db6e9bcd7b3b4e3df2a8c
Grant Holley <gvholley@gmail.com>
2021-05-16 11:37:01 +0900

    JS review

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-const add = (a: number, b: number = 1) => a + b;
+//const add = (a: number, b: number = 1) => a + b;
 
 const printOutput: (a: number | string) => void = output => console.log(output);
 
@@ -21,3 +21,21 @@ const person = {
 };
 
 const copiedPerson = {...person};
+
+const add = (...numbers: number[]) => {
+  numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+
+console.log(userName age);
