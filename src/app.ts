4092bc4afb575085cfad37bcc3750c58ba2c672d
Grant Holley <gvholley@gmail.com>
2021-05-16 11:23:30 +0900

    Spread operator

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+const add = (a: number, b: number = 1) => a + b;
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', event => console.log(event));
+}
+
+printOutput(add(5, 2));
+
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  name: 'Grant',
+  age: 30
+};
+
+const copiedPerson = {...person};
